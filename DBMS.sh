66769a3acdc0f6406e65d94c72069933f716df52
MoustafaElkady <moustafa.m.elkady@gmail.com>
2017-01-08 14:11:01 +0200

    Add manageDb&dropDb and FireMessage

--- DBMS.sh
+++ DBMS.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+#Moustafa Elkady <Moustafa.M.Elkady@gmail.com>
 
 createDb(){
     clear
@@ -7,22 +8,92 @@ createDb(){
     dbpath=$PWD/$name
     if [ -d $dbpath ]
     then
-        echo "The Database Already Exists"
+        firMessage "The Database Already Exists"
     else
-        mkdir $dbpath
-        echo "Database `$name` Created"
+        if mkdir $dbpath
+        then
+            manageDb $name
+        else
+            firMessage "Database creation error"
+        fi
     fi
 }
 
-echo "Choose:"
-echo "1. Create A New Database"
-echo "6. Exit"
+dropDb(){
+    clear
+    db=$PWD/$1;
+    echo "Drop $1 Database? (y/n)"
+    read confirm
+    if [ $confirm = "y" ]
+    then
+        if [ -d $db ] && rm -r $db
+        then
+            firMessage "Dropped Successfully"
+            break
+        else
+            firMessage "Dropping Error"
+        fi
+    fi
+}
 
-while read -r line
+manageDb(){
+    clear
+    echo "Database Name ?: "
+    if [ $1 ]; then
+        name=$1
+     else
+        read name
+    fi
+
+    dbpath=$PWD/$name
+    if [ -d $dbpath ]
+    then
+        while true
+        do
+            clear
+            echo "Database $name Selected"
+            echo "1. Browse"
+            echo "3. Drop"
+            echo "6. Back"
+
+            read -r line
+
+            case $line in
+                1) echo Browse ;;
+                3) dropDb $name;;
+                6) break ;;
+                *)
+                    echo Choise
+                    echo $line
+                ;;
+            esac
+        done
+
+    else
+        firMessage "The Database Is Not Exists"
+    fi
+}
+
+firMessage(){
+    clear
+    echo $1
+    echo "Press any key to back"
+    read confirm
+}
+
+while true
 do
+    clear
+    echo "Choose:"
+    echo "1. Create A New Database"
+    echo "2. Manage A Database"
+    echo "6. Exit"
+
+    read -r line
+
     case $line in
         1) createDb ;;
-        2) echo 2 ;;
+        2) manageDb ;;
         6) exit 1 ;;
         *)
             echo Choise
